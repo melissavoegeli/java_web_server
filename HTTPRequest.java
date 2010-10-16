a5eab40122dd7e2b4b6b14b7f62082dfce943689
melissavoegelidx <melissavoegelidx@gmail.com>
2010-10-16 15:50:41 -0500

    Second draft -- serves html, gif, and jpg requests

--- HTTPRequest.java
+++ HTTPRequest.java
@@ -28,6 +28,42 @@ public final class HTTPRequest implements Runnable
                         }   
                      
                 }
+                
+                private static void sendBytes(FileInputStream fis, OutputStream os) throws Exception
+                {
+                        
+                        //Construct a 1K buffer to hold bytes on their way to socket
+                        byte[] buffer = new byte[1024];
+                        int bytes = 0;
+                        
+                        //Copy requested file into the socket's output stream
+                        while((bytes = fis.read(buffer)) != -1) {
+                                os.write(buffer, 0, bytes);
+                        }
+                        
+                        
+                }
+                
+                private static String contentType(String fileName)
+                {
+                        
+                        if(fileName.endsWith(".htm") || fileName.endsWith(".html"))
+                        {
+                                return "text/html";
+                        }
+                        if(fileName.endsWith(".jpg"))
+                        {
+                                return "image/jpg";
+                        }
+                        if(fileName.endsWith(".gif"))
+                        {
+                                return "image/gif";
+                        }
+                        return "application/octet-stream";
+                        
+                        
+                }
+                
 
                 private void processRequest() throws Exception
                 {
@@ -54,13 +90,65 @@ public final class HTTPRequest implements Runnable
                 System.out.println(headerLine);
                 }
                 
+                StringTokenizer tokens = new StringTokenizer(requestLine);
+                tokens.nextToken();
+                String fileName = tokens.nextToken();
+                if (fileName.equals("/"))
+                    {
+                        fileName = "/index.html";
+                    }
+                fileName = "." + fileName;
+                
+                FileInputStream fis = null;
+                boolean fileExists = true;
+                try {
+                        fis = new FileInputStream(fileName);
+                }
+                catch (FileNotFoundException e) {
+                        fileExists = false;
+                }
+                
+                String statusLine = null;
+                String contentTypeLine = null;
+                String entityBody = null;
+                
+                
+                if (fileExists) {
+                        
+                        statusLine = "HTTP/1.0 200 OK" + CRLF;
+                        contentTypeLine = "Content-type: " +
+                                contentType( fileName ) + CRLF;
+                }
+                else {
+                        
+                        statusLine = "HTTP/1.0 404 NOT FOUND" + CRLF;
+                        contentTypeLine = "Content-type: text/html" + CRLF;
+                        entityBody = "<HTML>" +
+                                "<HEAD><TITLE>Not Found</TITLE></HEAD>" +
+                                "<BODY>Not Found</BODY></HTML>";
+                }
+                
+                dos.writeBytes(statusLine);
+                dos.writeBytes(contentTypeLine);
+                dos.writeBytes(CRLF);
+                
+                if (fileExists) {
+                
+                        sendBytes(fis, os);
+                        fis.close();
+                        
+
+                }
+                else {
+                        dos.writeBytes(entityBody);
+                }
+                
+                
                 os.close();
                 br.close();
                 socket.close();
                     
-                    
-                    
-                        
+                 
                 }
         
         
